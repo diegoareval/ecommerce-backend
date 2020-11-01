ff6909bdea46b53a50cc17303589e79ae78a8774
diego2000 <diego2000@gmail.com>
2020-10-31 20:06:22 -0600

    middleware para obtener un producto especifico

--- controllers/product.js
+++ controllers/product.js
@@ -5,6 +5,23 @@ const fs = require("fs")
 const Product = require("../models/product");
 const {errorHandler} = require("../helpers/dbErrorHandler")
 
+exports.productById = (req, res, next, id) =>{
+Product.findById(id).exec((error, product) =>{
+ if(error || !product){
+   res.status(400).json({
+         error: 'Product not found'
+       })
+ }
+ req.product = product
+ next()
+})
+}
+
+exports.read = (req, res) =>{
+  req.product.photo = undefined
+  return  res.json(req.product)
+}
+
 exports.create = (req, res) =>{
     let form = new formidable.IncomingForm();
     form.keepExtensions = true
--- routes/product.js
+++ routes/product.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 const {userById} = require("../controllers/user")
-const {create} = require("../controllers/product")
+const {create, productById, read} = require("../controllers/product")
 
+router.get("/product/:productId", read)
 router.post("/product/create/:userId",requireSignin,isAuth, isAdmin, create)
 router.param('userId', userById)
+router.param('productId', productById)
 
 module.exports = router;
