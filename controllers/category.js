bd75ba7cddd664cb760257ad552e8e1a36e47c72
diego2000 <diego2000@gmail.com>
2020-11-02 19:41:00 -0600

    crud de categorias finalizado con middleware

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,60 @@
 
 const Category = require("../models/category");
-const {errorHandler} = require("../helpers/dbErrorHandler")
+const {errorHandler} = require("../helpers/dbErrorHandler");
+
+exports.categoryById = (req, res, next, id) =>{
+   Category.findById(id).exec((error, category) =>{
+     if(error || !category){
+        res.status(400).json({
+       error: 'Category does not exist'
+     })
+     }
+     req.category = category
+     next()
+   })
+}
+
+exports.remove = (req, res) =>{
+ const category = req.category
+ category.remove((error, data) =>{
+   if(error || !data){
+     res.status(400).json({
+       error: errorHandler(err)
+     })
+   }
+   res.json({
+     message: "Category Deleted"
+   })
+ })
+}
+
+exports.list = (req, res) =>{
+ Category.find().exec((error, data)=>{
+   if(error || !data){
+     res.status(400).json({
+       error: errorHandler(err)
+     })
+   }
+   res.json(data)
+ })
+}
+
+exports.update = (req, res) =>{
+ const category = req.category
+ category.name = req.body.name
+ category.save((error, data) =>{
+   if(error || !data){
+     res.status(400).json({
+       error: errorHandler(err)
+     })
+   }
+   res.json(data)
+ })
+}
+
+exports.read = (req, res) =>{
+  return res.json(req.category)
+}
 
 exports.create = (req, res) =>{
 const category = new Category(req.body)
--- routes/category.js
+++ routes/category.js
@@ -3,9 +3,15 @@ const router = express.Router();
 
 const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 const {userById} = require("../controllers/user")
-const {create} = require("../controllers/category")
+const {create, categoryById, read, update, remove, list} = require("../controllers/category")
 
 router.post("/category/create/:userId",requireSignin,isAuth, isAdmin, create)
+router.put("/category/:categoryId/:userId",requireSignin,isAuth, isAdmin, update)
+router.delete("/category/:categoryId/:userId",requireSignin,isAuth, isAdmin, remove)
+router.get("/category/:categoryId", read)
+router.get("/categories", list)
+
+router.param("categoryId", categoryById)
 router.param('userId', userById)
 
 module.exports = router;
