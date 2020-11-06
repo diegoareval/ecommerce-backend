0e9d112edc39a05f6e6649d1479caff62611bc97
diego2000 <diego2000@gmail.com>
2020-11-06 17:50:19 -0600

    busqueda de productos

--- routes/product.js
+++ routes/product.js
@@ -3,12 +3,15 @@ const router = express.Router();
 
 const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 const {userById} = require("../controllers/user")
-const {create, productById, read, remove, update, list, listRelated} = require("../controllers/product")
+const {create, productById, read, remove, update, list, listRelated, listCategories, listBySearch} = require("../controllers/product")
 
+// route - make sure its post
+router.post("/products/by/search", listBySearch);
 router.delete("/product/:productId/:userId", requireSignin,isAuth, isAdmin, remove)
 router.put("/product/:productId/:userId", requireSignin,isAuth, isAdmin, update)
 router.get("/product/:productId", read)
 router.get("/products", list)
+router.get("/products/categories", listCategories)
 router.get("/products/related/:productId", listRelated)
 router.post("/product/create/:userId",requireSignin,isAuth, isAdmin, create)
 
