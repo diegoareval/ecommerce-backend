014e1a2a52f3dbab2658794dbbd2c85c3a6f912b
diego2000 <diego2000@gmail.com>
2020-11-07 10:10:04 -0600

    procesamiento de pagos

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const authRoutes = require("./routes/auth")
 const userRoutes = require('./routes/user')
 const categoryRoutes = require('./routes/category')
 const productRoutes = require('./routes/product')
+const brainTreeRoutes = require("./routes/braintree")
 // app
 const app = express();
 
@@ -37,6 +38,7 @@ app.use("/api",authRoutes)
 app.use("/api", userRoutes)
 app.use("/api", categoryRoutes)
 app.use("/api", productRoutes)
+app.use("/api", brainTreeRoutes)
 
 const port = process.env.PORT || 8000
 
---
+++ controllers/braintree.js
@@ -0,0 +1,43 @@
+
+const User = require("../models/user")
+require("dotenv").config()
+const braintree = require("braintree")
+
+const gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.BRAINTREE_MERCHANT_ID,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY
+})
+
+
+exports.generateToken = (req, res) =>{
+gateway.clientToken.generate({}, function(error, response){
+if(error){
+  res.status(500).send(error)
+}else{
+  res.send(response)
+}
+})
+
+}
+
+exports.processPayment = (req, res) =>{
+  let payloadFromTheClient = req.body.paymentMethodNonce
+  let clientAmount = req.body.amount
+
+  // make transaction
+  let newTransaction = gateway.transaction.sale({
+    amount: clientAmount,
+    paymentMethodNonce: payloadFromTheClient,
+    options: {
+      submitForSettlement: true
+    }
+  }, (error, result) =>{
+     if(error){
+       res.status(500).send(error)
+     }else{
+       res.json(result)
+     }
+  })
+}
